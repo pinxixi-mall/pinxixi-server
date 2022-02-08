e16e02d0d565a5ffcfeb43269de83672f8af5566
yetm <yetm@ibumobile.com>
2022-02-08 17:32:53 +0800

    init

---
+++ src/main/java/com/pinxixi/PinxixiServerApplication.java
@@ -0,0 +1,13 @@
+package com.pinxixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinxixiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinxixiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pinxixi/PinxixiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinxixi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinxixiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
